267c9da10b55bb20a032ac2c0a35a802fd17609d
chemadd <36247048+chemadd@users.noreply.github.com>
2018-02-18 20:24:24 +0100

    Add files via upload

--- PracticasUnidad01/Practica01_EvaluacionFinal_Funciones.py
+++ PracticasUnidad01/Practica01_EvaluacionFinal_Funciones.py
@@ -57,3 +57,55 @@ def obtenerSentimientosTweets(ficheroSentimientos, ficheroTweets, idioma):
         # print(tweet,tweetsAnalizar[tweet], valorSentimiento, listaSentimientos)
         sentimientosDict[tweet] = { 'texto':tweetsAnalizar[tweet], 'sentimiento':valorSentimiento, 'valoracion':listaSentimientos }
     return sentimientosDict
+
+def limpiaPalabra(palabra):
+    # Proceso que elimina de las palabras los caracteres extraños y devuelve palabra 'correctas'
+    # Pretende ser algo simple, se podria aplicar limpieza con la libreria unidecode y mayor logica al control
+    # de eliminacion de signos de puntuacion
+    # Tambien se podria devolver una palabra vacia para indicar que la cadena de caracteres no es una palabra
+    # valida
+    palabraLimpia = palabra.replace('#','')
+    palabraLimpia = palabraLimpia.replace('!','')
+    palabraLimpia = palabraLimpia.replace('?','')
+    palabraLimpia = palabraLimpia.replace('.','')
+    palabraLimpia = palabraLimpia.replace(',','')
+    palabraLimpia = palabraLimpia.replace('(','')
+    palabraLimpia = palabraLimpia.replace(')','')
+    
+    return palabraLimpia.lower()
+
+def obtenerPalabrasSentimientosTweets(ficheroSentimientos, ficheroTweets, idioma):
+    # Devuelve un diccionario asociando a cada palabra
+    # un valor calculado de los sentimientos de los tweets en los que aparece.
+    # El calculo es la media (redondeada) de los sentimientos de los tweets en 
+    # los que aparecen
+    resultado = obtenerSentimientosTweets(ficheroSentimientos, ficheroTweets, idioma)
+    # Buscamos las palabras de tweets que no aparecen en sentimientos.
+    # En resultado tenemos, para cada tweet, su texto, sentimiento y las palabras que 
+    # han generado ese sentimiento. Asociamos a cada palabra que no este en 'valoracion'
+    # el valor del sentimiento del tweet. Puesto que se va a hacer la media,
+    # se incrementa el valor del sentimiento de cada una y tambien el contador de
+    # apariciones, para poder hacer la media al final de su valoracion
+    valoracionPalabras = dict()
+    for idTweet in resultado:
+        # 1. Descomponemos las palabras del texto del tweet
+        listaPalabras = resultado[idTweet]['texto'].split()
+        # 2. A la valoracion de cada palabra le incrementamos el sentimiento del tweet
+        #    Incrementamos el contador de veces que aparece cada palabra para hacer
+        #    la media al final.
+        for palabra in listaPalabras:
+            palabraTransform = limpiaPalabra(palabra)
+            if palabraTransform in valoracionPalabras.keys():
+                valoracionPalabras[palabraTransform]['valoracion'] = valoracionPalabras[palabraTransform]['valoracion'] + resultado[idTweet]['sentimiento']
+                valoracionPalabras[palabraTransform]['conteo'] = valoracionPalabras[palabraTransform]['conteo'] + 1
+            elif len(palabra) > 1:
+                # Evitamos valorar signos de puntuacion o caracteres aislados
+                valoracionPalabras[palabraTransform] = {'valoracion':resultado[idTweet]['sentimiento'], 'conteo':1}             
+    # Ahora que ya tenemos la lista de palabras, obtenemos la media de sus sentimientos
+    mediaPalabras = dict()
+    for palabra in valoracionPalabras:
+        if valoracionPalabras[palabra]['conteo'] == 0:
+            mediaPalabras[palabra] = 0
+        else:
+            mediaPalabras[palabra] = round(valoracionPalabras[palabra]['valoracion']/valoracionPalabras[palabra]['conteo'])
+    return mediaPalabras
